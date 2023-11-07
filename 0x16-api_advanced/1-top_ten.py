37dfa5a5bf9651f4f6af76ad01f28ca4445bcf42
lkhaloufi soukaina <soukainanutrifeed@gmail.com>
2023-11-07 12:49:33 -0800

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and
+prints the titles of the first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the first 10 hot posts ."""
+    headers = {'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/BakrIbrahimi)'}
+    r = requests.get(f'https://www.reddit.com/r/{subreddit}/hot.json',
+                     headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json()
+        for child in data['data']['children'][:10]:
+            title = child['data']['title']
+            print(title)
+    else:
+        print("None")
